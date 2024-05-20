86574bfa500ded6990ad42b3a8361088c55db3ad
lmandar <mandarlone1999@gmail.com>
2024-05-20 23:08:37 +0530

    first commit

--- server/constants/constant.js
+++
@@ -1,9 +0,0 @@
-const clientid = "663289964846-jeureuum7jge0mjltl9qvk68uodt26a0.apps.googleusercontent.com"
-const clientsecret = "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"
-
-
-
-module.exports = {
-    clientid,
-    clientsecret
-}
